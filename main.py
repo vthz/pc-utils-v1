447d541dec21e8f53328f82968ef06fe8f6c1ff6
Shaheen Naiyer <snshohin@gmail.com>
2023-11-23 12:49:20 +0530

    Added logic for writing data in csv file

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import psutil
 import time
+import csv
+import os
 
 
 class PCUtils:
@@ -10,7 +12,7 @@ class PCUtils:
         partitions = psutil.disk_partitions(all=True)
         for partition in partitions:
             disk_usage = psutil.disk_usage(partition.device)
-            actual_data = [
+            single_partition_data = [
                 partition.device,
                 round(disk_usage.total / (1024 ** 3), 2),
                 round(disk_usage.used / (1024 ** 3), 2),
@@ -18,7 +20,7 @@ class PCUtils:
                 disk_usage.percent,
                 time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
             ]
-            self.disk_usage_list.append(actual_data)
+            self.disk_usage_list.append(single_partition_data)
 
     def get_disk_usage_stats(self):
         return self.disk_usage_list
@@ -26,18 +28,22 @@ class PCUtils:
 
 class FileWriter:
     def __init__(self):
-        pass
-
-    def process_drive_stats(self, drive_list):
-        self.write_to_csv(drive_list)
+        # timestamp = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime(time.time()))
+        csv_file_path = os.path.join(os.getcwd(), f"PCUtils_v1.csv")
+        self.file_name = csv_file_path
 
     def write_to_csv(self, data_to_write):
-        pass
+        try:
+            with open(self.file_name, mode='a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerows(data_to_write)
+        except Exception as e:
+            print(e)
 
 
 file_writer_obj = FileWriter()
 pc_utils_obj = PCUtils()
 pc_utils_obj.get_drive_storage_stats()
+
 drive_stats = pc_utils_obj.get_disk_usage_stats()
-print(drive_stats)
-# file_writer_obj.write_to_csv(drive_stats)
+file_writer_obj.write_to_csv(drive_stats)
